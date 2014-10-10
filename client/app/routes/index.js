132b45ed60740598b9ad5664a7ca0454ede0ffe3
codepreneur <contramundee@gmail.com>
2014-10-10 10:44:00 +0100

    removed content security policy

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function () {
-		return this.emberSync.find('user', 27);
-	},
+	// model: function () {
+	// 	return this.emberSync.find('user', 27);
+	// },
 	actions: {
 		save: function () {
       console.log('bluuu');
